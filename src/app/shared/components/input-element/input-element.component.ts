4a0439be28982a15c61fb0a4ec36cbe2890e3099
Jhay-Great <alemonai.jenef@gmail.com>
2025-06-30 23:59:35 +0000

    changes

---
+++ src/app/shared/components/input-element/input-element.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-element',
+  imports: [],
+  templateUrl: './input-element.component.html',
+  styleUrl: './input-element.component.scss'
+})
+export class InputElementComponent {
+
+}
